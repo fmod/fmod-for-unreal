4bc32200ac1edb8dea2fe308eec5933eefc9764a
Andrew Hockey <andrew@fmod.com>
2022-05-20 10:19:52 +1000

    Update for 2.02.07

--- FMODStudio/Source/FMODStudio/Private/FMODSettings.cpp
+++ FMODStudio/Source/FMODStudio/Private/FMODSettings.cpp
@@ -14,12 +14,34 @@
 //////////////////////////////////////////////////////////////////////////
 // UPaperRuntimeSettings
 
+inline EFMODPlatforms::Type CurrentPlatform()
+{
+    EFMODPlatforms::Type platform;
+#if defined(FMOD_PLATFORM_HEADER)
+    platform = FMODPlatform_CurrentPlatform();
+#elif WITH_EDITOR
+    platform = EFMODPlatforms::Editor;
+#elif PLATFORM_WINDOWS
+    platform = EFMODPlatforms::Windows;
+#elif PLATFORM_LINUX
+    platform = EFMODPlatforms::Linux;
+#elif PLATFORM_MAC
+    platform = EFMODPlatforms::Mac;
+#elif PLATFORM_ANDROID
+    platform = EFMODPlatforms::Android;
+#elif PLATFORM_IOS || PLATFORM_TVOS
+    platform = EFMODPlatforms::IOS;
+#endif
+    return platform;
+}
+
 UFMODSettings::UFMODSettings(const FObjectInitializer &ObjectInitializer)
     : Super(ObjectInitializer)
 {
     MasterBankName = TEXT("Master");
     BankOutputDirectory.Path = TEXT("FMOD");
     OutputFormat = EFMODSpeakerMode::Surround_5_1;
+    OutputType = EFMODOutput::TYPE_AUTODETECT;
     ContentBrowserPrefix = TEXT("/Game/FMOD/");
     bLoadAllBanks = true;
     bLoadAllSampleData = false;
@@ -153,3 +175,62 @@ UFMODSettings::EProblem UFMODSettings::Check() const
     return Okay;
 }
 #endif // WITH_EDITOR
+
+EFMODSpeakerMode::Type UFMODSettings::GetSpeakerMode() const
+{
+    return Platforms.Contains(CurrentPlatform()) ? Platforms.Find(CurrentPlatform())->SpeakerMode : OutputFormat;
+}
+
+EFMODOutput::Type UFMODSettings::GetOutputType() const
+{
+    return Platforms.Contains(CurrentPlatform()) ? Platforms.Find(CurrentPlatform())->OutputType : OutputType;
+}
+
+int32 UFMODSettings::GetSampleRate() const
+{
+    return Platforms.Contains(CurrentPlatform()) ? Platforms.Find(CurrentPlatform())->SampleRate : SampleRate;
+}
+
+int32 UFMODSettings::GetMemoryPoolSize() const
+{
+    return (Platforms.Contains(CurrentPlatform()) ? Platforms.Find(CurrentPlatform())->CustomPoolSize : 0);
+}
+
+int32 UFMODSettings::GetRealChannelCount() const
+{
+    return Platforms.Contains(CurrentPlatform()) ? Platforms.Find(CurrentPlatform())->RealChannelCount : RealChannelCount;
+}
+
+bool UFMODSettings::SetCodecs(FMOD_ADVANCEDSETTINGS& advSettings) const
+{
+    const FFMODPlatformSettings* platform = Platforms.Find(CurrentPlatform());
+    if (platform == nullptr)
+    {
+        return false;
+    }
+    TMap<TEnumAsByte<EFMODCodec::Type>, int32> codecList = platform->Codecs;
+
+    for (const TPair<TEnumAsByte<EFMODCodec::Type>, int32>& pair : codecList)
+    {
+        switch (pair.Key)
+        {
+        case EFMODCodec::XMA:
+            advSettings.maxXMACodecs = pair.Value;
+            break;
+        case EFMODCodec::AT9:
+            advSettings.maxAT9Codecs = pair.Value;
+            break;
+        case EFMODCodec::FADPCM:
+            advSettings.maxFADPCMCodecs = pair.Value;
+            break;
+        case EFMODCodec::OPUS:
+            advSettings.maxOpusCodecs = pair.Value;
+            break;
+        case EFMODCodec::VORBIS:
+        default:
+            advSettings.maxVorbisCodecs = pair.Value;
+            break;
+        }
+    }
+    return true;
+}
