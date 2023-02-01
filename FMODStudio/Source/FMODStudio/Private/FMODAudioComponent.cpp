7908b8cb7df4aaa8eb7a7f0c259a516b4748a751
Andrew Hockey <andrew@fmod.com>
2023-02-01 15:08:09 +1100

    Update for 2.02.12

--- FMODStudio/Source/FMODStudio/Private/FMODAudioComponent.cpp
+++ FMODStudio/Source/FMODStudio/Private/FMODAudioComponent.cpp
@@ -1,4 +1,4 @@
-// Copyright (c), Firelight Technologies Pty, Ltd. 2012-2022.
+// Copyright (c), Firelight Technologies Pty, Ltd. 2012-2023.
 
 #include "FMODAudioComponent.h"
 #include "FMODStudioModule.h"
@@ -18,30 +18,43 @@
 
 UFMODAudioComponent::UFMODAudioComponent(const FObjectInitializer &ObjectInitializer)
     : Super(ObjectInitializer)
+    , Event(nullptr)
+    , bEnableTimelineCallbacks(false) // Default OFF for efficiency
+    , bAutoDestroy(false)
+    , bStopWhenOwnerDestroyed(true)
+    , bApplyAmbientVolumes(false)
+    , bApplyOcclusionParameter(false)
+    , StudioInstance(nullptr)
+    , bDefaultParameterValuesCached(false)
+    , Module(nullptr)
+    , InteriorLastUpdateTime(0.0f)
+    , SourceInteriorVolume(0.0f)
+    , SourceInteriorLPF(0.0f)
+    , CurrentInteriorVolume(0.0f)
+    , CurrentInteriorLPF(0.0f)
+    , AmbientVolume(0.0f)
+    , AmbientLPF(0.0f)
+    , LastVolume(1.0f)
+    , LastLPF(MAX_FILTER_FREQUENCY)
+    , wasOccluded(false)
+    , OcclusionID()
+    , AmbientVolumeID()
+    , AmbientLPFID()
+    , ProgrammerSound(nullptr)
+    , NeedDestroyProgrammerSoundCallback(false)
+    , EventLength(0)
 {
-    bAutoDestroy = false;
     bAutoActivate = true;
-    bEnableTimelineCallbacks = false; // Default OFF for efficiency
-    bStopWhenOwnerDestroyed = true;
     bNeverNeedsRenderUpdate = true;
     bWantsOnUpdateTransform = true;
+
 #if WITH_EDITORONLY_DATA
     bVisualizeComponent = true;
 #endif
-    bApplyOcclusionParameter = false;
-    bDefaultParameterValuesCached = false;
 
     PrimaryComponentTick.bCanEverTick = true;
     PrimaryComponentTick.TickGroup = TG_PrePhysics;
     PrimaryComponentTick.bStartWithTickEnabled = false;
-
-    StudioInstance = nullptr;
-    ProgrammerSound = nullptr;
-
-    LastLPF = MAX_FILTER_FREQUENCY;
-    LastVolume = 1.0f;
-    Module = nullptr;
-    wasOccluded = false;
 
     for (int i = 0; i < EFMODEventProperty::Count; ++i)
     {
@@ -128,6 +141,7 @@ void UFMODAudioComponent::PostEditChangeProperty(FPropertyChangedEvent &e)
         ParameterCache.Empty();
         bDefaultParameterValuesCached = false;
     }
+    UpdateCachedParameterValues();
 
 #if WITH_EDITORONLY_DATA
     UpdateSpriteTexture();
@@ -303,27 +317,54 @@ void UFMODAudioComponent::ApplyVolumeLPF()
     }
 }
 
+bool UFMODAudioComponent::ShouldCacheParameter(const FMOD_STUDIO_PARAMETER_DESCRIPTION& ParameterDescription)
+{
+    const UFMODSettings& Settings = *GetDefault<UFMODSettings>();
+
+    if (((ParameterDescription.flags & FMOD_STUDIO_PARAMETER_GLOBAL) == 0) &&
+        (ParameterDescription.type == FMOD_STUDIO_PARAMETER_GAME_CONTROLLED) &&
+        ParameterDescription.name != Settings.OcclusionParameter &&
+        ParameterDescription.name != Settings.AmbientVolumeParameter &&
+        ParameterDescription.name != Settings.AmbientLPFParameter)
+    {
+        return true;
+    }
+    return false;
+}
+
 void UFMODAudioComponent::CacheDefaultParameterValues()
 {
     if (Event)
     {
-        const UFMODSettings &Settings = *GetDefault<UFMODSettings>();
         TArray<FMOD_STUDIO_PARAMETER_DESCRIPTION> ParameterDescriptions;
         Event->GetParameterDescriptions(ParameterDescriptions);
         for (const FMOD_STUDIO_PARAMETER_DESCRIPTION &ParameterDescription : ParameterDescriptions)
         {
-            if (!ParameterCache.Find(ParameterDescription.name) && 
-                ((ParameterDescription.flags & FMOD_STUDIO_PARAMETER_GLOBAL) == 0) &&
-                (ParameterDescription.type == FMOD_STUDIO_PARAMETER_GAME_CONTROLLED) &&
-                ParameterDescription.name != Settings.OcclusionParameter && 
-                ParameterDescription.name != Settings.AmbientVolumeParameter && 
-                ParameterDescription.name != Settings.AmbientLPFParameter)
+            if (ShouldCacheParameter(ParameterDescription))
             {
                 ParameterCache.Add(ParameterDescription.name, ParameterDescription.defaultvalue);
             }
         }
+        bDefaultParameterValuesCached = true;
     }
-    bDefaultParameterValuesCached = true;
+}
+
+void UFMODAudioComponent::UpdateCachedParameterValues()
+{
+    if (bDefaultParameterValuesCached)
+    {
+        TArray<FMOD_STUDIO_PARAMETER_DESCRIPTION> ParameterDescriptions;
+        Event->GetParameterDescriptions(ParameterDescriptions);
+        for (const FMOD_STUDIO_PARAMETER_DESCRIPTION& ParameterDescription : ParameterDescriptions)
+        {
+            if (ParameterCache.Find(ParameterDescription.name) && !ShouldCacheParameter(ParameterDescription))
+            {
+                ParameterCache.Remove(ParameterDescription.name);
+                FString paramName(ParameterDescription.name);
+                UE_LOG(LogFMOD, Warning, TEXT("Parameter '%s' is driven elsewhere and cannot be added here."), *paramName)
+            }
+        }
+    }
 }
 
 void UFMODAudioComponent::OnUnregister()
@@ -338,6 +379,7 @@ void UFMODAudioComponent::OnUnregister()
 
 void UFMODAudioComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
+    Super::EndPlay(EndPlayReason);
     bool shouldStop = false;
 
     switch (EndPlayReason)
--- FMODStudio/Source/FMODStudio/Private/FMODBankLookup.cpp
+++ FMODStudio/Source/FMODStudio/Private/FMODBankLookup.cpp
@@ -1,1 +1,6 @@
 #include "FMODBankLookup.h"
+
+UFMODBankLookup::UFMODBankLookup(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+    , DataTable(nullptr)
+{}
