062a673beb47d0f6b83a2b4c8392cbd00a96c1db
Andrew Hockey <andrew@fmod.com>
2025-02-11 15:08:32 +1100

    Update for 2.02.27

--- FMODStudio/Source/FMODAudioLink/Private/FMODAudioLinkSettings.cpp
+++ FMODStudio/Source/FMODAudioLink/Private/FMODAudioLinkSettings.cpp
@@ -1,4 +1,4 @@
-// Copyright (c), Firelight Technologies Pty, Ltd. 2024-2024.
+// Copyright (c), Firelight Technologies Pty, Ltd. 2025-2025.
 
 #include "FMODAudioLinkSettings.h"
 #include "FMODAudioLinkFactory.h"
