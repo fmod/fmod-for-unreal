646179041a387804ab1a9499c449da24c0d50e09
fmod-cameron <cameron@fmod.com>
2025-08-27 14:19:39 +1000

    Fixed UE5.6.1 FMODCallbackHandler build issue.

--- FMODStudio/Source/FMODStudio/Public/FMODCallbackHandler.h
+++ FMODStudio/Source/FMODStudio/Public/FMODCallbackHandler.h
@@ -2,27 +2,4 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
-#include "UObject/Interface.h"
-#include "fmod_studio.hpp"
-#include "FMODCallbackHandler.generated.h"
-
-/**
- * This class is required for Unreal reflection, but does not need to be modified.
- */
-UINTERFACE(MinimalAPI)
-class UFMODCallbackHandler : public UInterface
-{
-	GENERATED_BODY()
-};
-
-class FMODSTUDIO_API IFMODCallbackHandler
-{
-    GENERATED_BODY()
-
-public:
-    /**
-     * Method called just before the FMOD System initializes.
-     */
-    virtual void PreInitialize(FMOD::Studio::System*) = 0;
-};
+#include "FMOD/FMODCallbackHandler.h"
