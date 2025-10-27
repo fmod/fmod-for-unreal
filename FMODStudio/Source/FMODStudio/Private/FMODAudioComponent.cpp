07579dc9d8fae4d9b54ed36004d12850720b3f46
Andrew Hockey <andrew@fmod.com>
2025-10-28 10:44:00 +1100

    Update for 2.02.31

--- FMODStudio/Source/FMODStudio/Private/FMODAudioComponent.cpp
+++ FMODStudio/Source/FMODStudio/Private/FMODAudioComponent.cpp
@@ -99,7 +99,7 @@ void UFMODAudioComponent::OnRegister()
     if (IsActive() && bAutoActivate)
     {
         FMOD_STUDIO_PLAYBACK_STATE state = FMOD_STUDIO_PLAYBACK_STOPPED;
-        if (StudioInstance->isValid())
+        if (StudioInstance && StudioInstance->isValid())
         {
             StudioInstance->getPlaybackState(&state);
         }
@@ -863,7 +863,7 @@ void UFMODAudioComponent::ResumeInternal(PauseContext Pauser)
 void UFMODAudioComponent::Stop()
 {
     UE_LOG(LogFMOD, Verbose, TEXT("UFMODAudioComponent %p Stop"), this);
-    if (StudioInstance->isValid())
+    if (StudioInstance && StudioInstance->isValid())
     {
         StudioInstance->stop(FMOD_STUDIO_STOP_ALLOWFADEOUT);
     }
@@ -893,7 +893,7 @@ void UFMODAudioComponent::ReleaseEventCache()
 
 void UFMODAudioComponent::ReleaseEventInstance()
 {
-    if (StudioInstance->isValid())
+    if (StudioInstance && StudioInstance->isValid())
     {
         if (NeedDestroyProgrammerSoundCallback)
         {
