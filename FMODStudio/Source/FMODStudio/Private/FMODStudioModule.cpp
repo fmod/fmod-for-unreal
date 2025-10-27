e1a7e1c644835373c302216c54ff5ed0af00ce1a
Andrew Hockey <andrew@fmod.com>
2025-10-28 10:50:44 +1100

    Update for 2.03.10

--- FMODStudio/Source/FMODStudio/Private/FMODStudioModule.cpp
+++ FMODStudio/Source/FMODStudio/Private/FMODStudioModule.cpp
@@ -232,6 +232,7 @@ public:
 
     virtual const FFMODListener &GetNearestListener(const FVector &Location) override;
 
+    virtual float DistanceSquaredToNearestListener(const FVector &location) override;
     virtual bool HasListenerMoved() override;
 
     virtual void SetSystemPaused(bool paused) override;
@@ -631,10 +632,15 @@ void FFMODStudioModule::CreateStudioSystem(EFMODSystemContext::Type Type)
         outputType = FMOD_OUTPUTTYPE_WAVWRITER;
         InitData = (void *)WavWriterDestUTF8.Get();
     }
+    else if (Type == EFMODSystemContext::Editor)
+    {
+        outputType = FMOD_OUTPUTTYPE_NOSOUND;
+    }
     else
     {
         outputType = ConvertOutputType(Settings.GetOutputType());
     }
+
     verifyfmod(lowLevelSystem->setOutput(outputType));
 
     int DriverIndex = 0;
@@ -704,11 +710,14 @@ void FFMODStudioModule::CreateStudioSystem(EFMODSystemContext::Type Type)
     if (Type == EFMODSystemContext::Runtime)
     {
         advSettings.profilePort = Settings.LiveUpdatePort;
+        advSettings.maxSpatialObjects = 0;
     }
     else if (Type == EFMODSystemContext::Auditioning)
     {
         advSettings.profilePort = Settings.EditorLiveUpdatePort;
+        advSettings.maxSpatialObjects = 8;
     }
+
     advSettings.randomSeed = FMath::Rand();
     verifyfmod(lowLevelSystem->setAdvancedSettings(&advSettings));
 
@@ -910,6 +919,8 @@ bool FFMODStudioModule::Tick(float DeltaTime)
         SET_DWORD_STAT(STAT_FMOD_Real_Channels, realChannels);
         SET_DWORD_STAT(STAT_FMOD_Total_Channels, channels);
 
+        UFMODAudioComponent::UpdateActiveComponents();
+
         verifyfmod(ClockSinks[EFMODSystemContext::Runtime]->LastResult);
     }
     if (ClockSinks[EFMODSystemContext::Editor].IsValid())
@@ -1019,6 +1030,16 @@ const FFMODListener &FFMODStudioModule::GetNearestListener(const FVector &Locati
         }
     }
     return Listeners[BestListener];
+}
+
+float FFMODStudioModule::DistanceSquaredToNearestListener(const FVector& Location)
+{
+    if (ListenerCount == 0)
+    {
+        return FLT_MAX;
+    }
+
+    return FVector::DistSquared(Location, GetNearestListener(Location).Transform.GetTranslation());
 }
 
 // Partially copied from FAudioDevice::SetListener
