c6566cd56500f8b1de844376dea5cd94e13bf140
Andrew Hockey <andrew@fmod.com>
2025-11-18 21:55:46 +1100

    Update for 2.03.11

--- FMODStudio/Source/FMODStudio/Public/FMOD/fmod_common.h
+++ FMODStudio/Source/FMODStudio/Public/FMOD/fmod_common.h
@@ -55,7 +55,7 @@ typedef struct FMOD_ASYNCREADINFO  FMOD_ASYNCREADINFO;
     FMOD constants
 */
 #define FMOD_VERSION    0x00020311                     /* 0xaaaabbcc -> aaaa = product version, bb = major version, cc = minor version.*/
-#define FMOD_BUILDNUMBER 158337
+#define FMOD_BUILDNUMBER 121212
 
 typedef unsigned int FMOD_DEBUG_FLAGS;
 #define FMOD_DEBUG_LEVEL_NONE                       0x00000000
